b6b77fd236523cf0df8577a31fd3eea04f5e2eaa
Immo Landwerth <immol@microsoft.com>
2019-06-20 20:10:49 -0700

    Initial commit

---
+++ ready-to-run/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ready_to_run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
